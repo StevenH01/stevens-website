dd1bb89d6149f5050bb1d6422b8f49da1e29be3b
StevenH01 <105683557+StevenH01@users.noreply.github.com>
2024-01-08 22:28:46 -0800

    created contact info in new tab and adjusted divs to mui components

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import HomePage from './components/HomePage';
 import { ThemeProvider } from './ThemeContext';
 import './App.css';
+import MainPage from './components/MainPage';
 
 function App() {
   return (
     <ThemeProvider>
       <HomePage />
+      <MainPage />
     </ThemeProvider>
   );
 }
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Button, Stack, IconButton } from '@mui/material';
+import { Button, IconButton, Box } from '@mui/material';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import { useTheme } from '../ThemeContext';
+import Contact from "./Contact";
 
 const HomePage = () => {
   const { theme, toggleTheme } = useTheme();
@@ -14,31 +15,34 @@ const HomePage = () => {
   };
 
   return (
-    <div style={{ background: theme === 'dark' ? '#333' : '#fff', padding: '10px', textAlign: 'left', color: theme === 'dark' ? '#fff' : '#333', position: 'relative' }}>
-      {/* Divider Line */}
-      <div style={{ borderBottom: '1px solid black', width: '100%', position: 'absolute', top: '75%', zIndex: 1 }}></div>
+    <Box sx={{ background: theme === 'dark' ? '#333' : '#fff', padding: '10px', textAlign: 'left', color: theme === 'dark' ? '#fff' : '#333', position: 'relative' }}>
+      {/* Boxider Line */}
+      <Box sx={{ borderBottom: '1px solid black', width: '100%', position: 'absolute', top: '75%', zIndex: 1 }}></Box>
 
-      <div style={{ display: 'flex', alignItems: 'center', marginTop: '-1px' }}>
-        <div style={{ marginRight: '10px' }}>
+      <Box sx={{ display: 'flex', alignItems: 'center', marginTop: '-1px' }}>
+        <Box sx={{ marginRight: '10px' }}>
           <GitHubIcon />
-        </div>
+        </Box>
         <Button color="inherit" onClick={() => openInNewTab('https://github.com/StevenH01')}>
           GitHub
         </Button>
 
-        <div style={{ marginLeft: '20px', marginRight: '10px' }}>
-          <LinkedInIcon style={{ color: '#0077b5' }} />
-        </div>
+        <Box sx={{ marginLeft: '20px', marginRight: '10px' }}>
+          <LinkedInIcon sx={{ color: '#0077b5' }} />
+        </Box>
         <Button color="inherit" onClick={() => openInNewTab('https://www.linkedin.com/in/steven-ho-31a667211/')}>
           LinkedIn
         </Button>
-        <div style={{ position: 'absolute', top: 0, right: 0 }}>
+        <Box>
+          <Contact />
+        </Box>
+        <Box sx={{ position: 'absolute', top: 0, right: 0 }}>
           <IconButton color="inherit" onClick={toggleTheme}>
             {theme === 'dark' ? <Brightness4Icon /> : <Brightness7Icon />}
           </IconButton>
-        </div>
-      </div>
-    </div>
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
