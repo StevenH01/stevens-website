7b06a9a9fbd592b0fdcbda91f628f3e04bf5f538
Steven Ho <steven09ho@gmail.com>
2024-01-16 18:51:02 -0800

    updated and moved files for nextjs support

---
+++ pages/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Box } from '@mui/material'
+
+const HomePage = () => {
+
+  return (
+    <Box>
+      This is the body of my website
+    </Box>
+  )
+}
+
+export default HomePage;
