0d70618dbdbc3388f37d1b461d65c9eedcb32be8
Steven Ho <steven09ho@gmail.com>
2024-02-04 16:05:14 -0800

    added pages for future use

---
+++ src/components/AboutPage.js
@@ -0,0 +1,15 @@
+// AboutPage.js
+import React from 'react';
+import { Typography } from '@mui/material';
+
+const AboutPage = () => {
+  return (
+    <div>
+      <Typography variant="h2">About Page Content</Typography>
+      {/* Add your about page content here */}
+    </div>
+  );
+}
+
+export default AboutPage;
+
