1c03a5046a4de32d7cd1090535a42ae2b3fc2a37
StevenH01 <105683557+StevenH01@users.noreply.github.com>
2024-02-05 23:23:39 -0800

    reverting to just showing tabs insttead of on hover

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -4,52 +4,58 @@ import Header from './Header';
 import AboutPage from './AboutPage';
 import ResumePage from './ResumePage';
 import ProjectPage from './ProjectsPage';
-import HomePage from './HomePage'; // Import the HomePage component
+import HomePage from './HomePage';
 
 const MainPage = () => {
   const [selectedTab, setSelectedTab] = useState(0);
-  const [showTabs, setShowTabs] = useState(false);
 
   const handleTabChange = (event, newValue) => {
     setSelectedTab(newValue);
-  };
-
-  const handleMouseEnter = () => {
-    setShowTabs(true);
-  };
-
-  const handleMouseLeave = () => {
-    setShowTabs(false);
   };
 
   return (
     <Grid sx={{ minHeight: '100vh', minWidth: '100vw' }}>
       <Header />
       <Container sx={{ background: '#333', minHeight: '100vh', minWidth: '100vw', color: '#fff', position: 'relative' }}>
-        <Grid grid item={3} sx={{ position: 'absolute', left: 0, top: '10px', maxWidth: '200px', color: 'inherit', textAlign: 'left' }} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-          {showTabs && (
-            <Tabs orientation="vertical" value={selectedTab} onChange={handleTabChange} >
-              <Tab label="Home" sx={{ color: 'white' }} />
-              <Tab label="About" sx={{ color: 'white' }} />
-              <Tab label="Resume" sx={{ color: 'white' }} />
-              <Tab label="Projects" sx={{ color: 'white' }} />
-            </Tabs>
-          )}
+        <Grid
+          item
+          xs={3} // Adjust the width as needed
+          sx={{
+            position: 'absolute',
+            left: 0,
+            top: 0,
+            bottom: 0,
+            maxWidth: '200px', // Adjust the width as needed
+            color: 'inherit',
+            textAlign: 'left',
+            zIndex: 1,
+          }}
+        >
+          <Tabs orientation="vertical" value={selectedTab} onChange={handleTabChange}>
+            <Tab label="Home" sx={{ color: 'white' }} />
+            <Tab label="About" sx={{ color: 'white' }} />
+            <Tab label="Resume" sx={{ color: 'white' }} />
+            <Tab label="Projects" sx={{ color: 'white' }} />
+          </Tabs>
         </Grid>
-        <Grid grid item={6} sx={{
-          background: '#333',
-          height: '100%',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          padding: '20px',
-          '@media (min-width:600px)': {
-            padding: '50px'
-          },
-          '@media (min-width:900px)': {
-            padding: '100px'
-          }
-        }}>
+        <Grid
+          item
+          xs={9} // Adjust the width as needed
+          sx={{
+            background: '#333',
+            height: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            padding: '20px',
+            '@media (min-width:600px)': {
+              padding: '50px',
+            },
+            '@media (min-width:900px)': {
+              padding: '100px',
+            },
+          }}
+        >
           {selectedTab === 0 && <HomePage />}
           {selectedTab === 1 && <AboutPage />}
           {selectedTab === 2 && <ResumePage />}
@@ -58,6 +64,6 @@ const MainPage = () => {
       </Container>
     </Grid>
   );
-}
+};
 
 export default MainPage;
