ce598373a73cad1462e987ed89e5cec036dd81af
StevenH01 <105683557+StevenH01@users.noreply.github.com>
2024-03-13 21:46:09 -0700

    resetting to build back from scratch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
-import MainPage from './components/MainPage';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-      <><MainPage /></>
+      <div>
+        <NavBar />
+      </div>
   );
 }
 
