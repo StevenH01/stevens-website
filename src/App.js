2ca0ddfc5115990ea2085de027d97b5577e3d0b1
StevenH01 <105683557+StevenH01@users.noreply.github.com>
2024-03-20 22:27:00 -0700

    updated routing for display

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import './App.css';
-import NavBar from './components/NavBar';
+import Hero from "./components/Hero";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
       <div>
         <NavBar />
+        <Hero />
       </div>
   );
 }
