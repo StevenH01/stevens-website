a24896a6ecc3fce4295a658160311844160e2a3d
StevenH01 <105683557+StevenH01@users.noreply.github.com>
2024-03-20 22:26:46 -0700

    added navbar and hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { TypeAnimation } from 'react-type-animation';
+import heroimage from '../images/selfie.png';
+
+const Hero = () => {
+  return (
+    <div className='grid grid-cols-1 sm:grid-cols-3 gap-8 max-w-[1200px] mx-auto py-8 bg-black'>
+      Hello World
+    </div>
+  )
+}
+
+export default Hero;
