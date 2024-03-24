c1fb02981c355a3a02732102f6c7aa8b1f02752f
StevenH01 <105683557+StevenH01@users.noreply.github.com>
2024-03-23 17:08:26 -0700

    starting hero/mainpage section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,8 +4,14 @@ import heroimage from '../images/selfie.png';
 
 const Hero = () => {
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-3 gap-8 max-w-[1200px] mx-auto py-8 bg-black'>
-      Hello World
+    <div className='grid grid-cols-1 sm:grid-cols-3 gap-8 max-w-[1200px] h-[70vh] mx-auto py-8 bg-black'>
+      <div className='col-span-1 my-auto mx-auto w-[300px] h-auto lg:w-[400px]'>
+        <img src={heroimage} alt='hero image' />
+      </div>
+
+      <div className='col-span-2 px-5'>
+        Text
+      </div>
     </div>
   )
 }
