59520556bf2b64784cafb55d48908b57ec473945
Steven Ho <steven09ho@gmail.com>
2024-03-31 14:31:48 -0700

    created footer section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 import Projects from "./components/Projects";
@@ -14,6 +15,7 @@ function App() {
         <About />
         <Projects />
         <Contact />
+        <Footer />
       </div>
   );
 }
