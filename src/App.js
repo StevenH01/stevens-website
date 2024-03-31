6c7b94a9376c7b9d78923ba22198e9e844d1b97e
Steven Ho <steven09ho@gmail.com>
2024-03-31 14:28:22 -0700

    created contace me section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 import Projects from "./components/Projects";
@@ -12,6 +13,7 @@ function App() {
         <Skills />
         <About />
         <Projects />
+        <Contact />
       </div>
   );
 }
