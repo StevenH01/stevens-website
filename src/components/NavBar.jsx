947e9484a2378b8898842b86bb4678633d2fa03c
Steven Ho <steven09ho@gmail.com>
2024-03-31 14:56:51 -0700

    fixed name display

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,7 +10,7 @@ const NavBar = () => {
 
   return (
     <div className='border border-gray-600 bg-black text-gray-400 h-[100px] max-w-[1200px] mx-auto flex justify-between items-center'>
-      <h1 className='text-3xl font-bold primary-color ml-4'>S. Ho</h1>
+      <h1 className='text-3xl font-bold primary-color ml-4'>Steven Ho</h1>
       <ul className='hidden md:flex'>
         <li className='p-5'><a href='#about'>About</a></li>
         <li className='p-5'><a href='#projects'>Projects</a></li>
